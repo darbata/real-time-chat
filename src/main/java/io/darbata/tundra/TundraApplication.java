f03cccc8564b4bf73900133102db114640a24b90
Darren Batarina <batarina.darren@gmail.com>
2026-04-01 23:40:51 +1100

    init

---
+++ src/main/java/io/darbata/tundra/TundraApplication.java
@@ -0,0 +1,13 @@
+package io.darbata.tundra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TundraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TundraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/darbata/tundra/TundraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.darbata.tundra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TundraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

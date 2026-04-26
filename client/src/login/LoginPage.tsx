6fbf2f68487552f0ab8f5064221344df271256db
Darren Batarina <batarina.darren@gmail.com>
2026-04-26 15:42:39 +1000

    structure files in terms of pages

---
+++ client/src/login/LoginPage.tsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+import {useNavigate} from "react-router";
+import {useUser} from "../UserContext.tsx";
+
+export default function LoginPage() {
+    const [login, setLogin] = useState("");
+    const navigate = useNavigate();
+    const {username, setUsername} = useUser();
+
+    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setUsername(login);
+    }
+
+    if (username != "") navigate("/chat")
+
+    return  (
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <label htmlFor="login">Login</label>
+            <input type="text" value={login} onChange={(e) => {setLogin(e.target.value)}}/>
+        </form>
+    )
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from 'react-router'
+import ChatPage from "./chat/ChatPage.tsx";
+import LoginPage from "./login/LoginPage.tsx";
+import {UserProvider} from "./UserContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <UserProvider>
+          <BrowserRouter>
+              <Routes>
+                  <Route path={"/"} element={<LoginPage />} />
+                  <Route path={"/chat"} element={<ChatPage />} />
+              </Routes>
+          </BrowserRouter>
+      </UserProvider>
   </StrictMode>,
 )

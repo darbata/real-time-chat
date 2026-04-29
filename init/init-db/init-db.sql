ecd5ceaafb93c344da090f449edb12d0f4e52e7a
Darren Batarina <batarina.darren@gmail.com>
2026-04-30 08:11:56 +1000

    re-add last_message_sent_at

--- init/init-db/init-db.sql
+++ init/init-db/init-db.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS user_conversations (
     user_id TEXT NOT NULL REFERENCES users(id),
     conversation_id BIGINT NOT NULL REFERENCES conversations(id),
     joined_at TIMESTAMP DEFAULT NOW(),
+    last_message_at TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (user_id, conversation_id)
 );
 

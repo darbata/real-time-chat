d45d74d23b212d321a5ab045f4b37111a2fc6f7b
Darren Batarina <batarina.darren@gmail.com>
2026-04-30 08:08:24 +1000

    add view with array of participants to db

--- init/init-db/init-db.sql
+++ init/init-db/init-db.sql
@@ -10,6 +10,14 @@ CREATE TABLE IF NOT EXISTS user_conversations (
     user_id TEXT NOT NULL REFERENCES users(id),
     conversation_id BIGINT NOT NULL REFERENCES conversations(id),
     joined_at TIMESTAMP DEFAULT NOW(),
-    last_message_at TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (user_id, conversation_id)
 );
+
+CREATE VIEW conversation_with_participants AS
+SELECT
+    conversations.id,
+    array_agg(users.id) AS participant_ids
+FROM conversations
+JOIN user_conversations ON user_conversations.conversation_id = conversations.id
+JOIN users on users.id = user_conversations.user_id
+GROUP BY conversations.id;
